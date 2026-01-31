e7e26e14e109b5b01092d9ad6cf996b32d25ed98
Tair Baimbetov <andreyyyyyiop@gmail.com>
2026-01-31 08:27:23 +0500

    Add classes

--- third.py
+++ third.py
@@ -1,3 +1,13 @@
-print(10 > 9)
-print(10 == 9)
-print(10 < 9)
+class Hogwarts:
+    faculty = "empty"
+    founder = "empty"
+    student_number = "empty"
+    def set_info(self, faculty_name, founder_name, st_num):
+        self.faculty = faculty_name
+        self.founder = founder_name
+        self.student_number = st_num
+    def get_info(self):
+        print(self.faculty, self.founder, self.student_number)
+_Harry_potter = Hogwarts()
+_Harry_potter.set_info("sdfsf","fdsfwd",42)
+_Harry_potter.get_info()
